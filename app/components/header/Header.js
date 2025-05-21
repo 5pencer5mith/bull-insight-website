0b2cd881d6f5c7c27bd8dcc5b34d1af5d691d264
Spence <120441242+5pencer5mith@users.noreply.github.com>
2025-05-21 14:16:30 -0600

    Update Header.js

--- app/components/header/Header.js
+++ app/components/header/Header.js
@@ -8,10 +8,7 @@ import { useState, useEffect } from "react";
 // import { menuOutline, closeOutline } from "ionicons/icons";
 import Link from "next/link";
 
-import { 
-  Menu,
-  X
-} from 'lucide-react'
+import { Menu, X } from "lucide-react";
 
 // Styles
 import styles from "./header.module.css";
@@ -36,25 +33,41 @@ export default function Header() {
   };
 
   return (
-    <div className="flex flex-end bg-white/75 text-stone-900 h-16 items-center p-4 shadow-lg backdrop-blur-xl backdrop-opacity-60 sticky top-0 z-50">
-      <div className="flex flex-1 items-center text-xl">
-        <Link href="/"><span className=" text-stone-900">Bull</span><span className="font-bold text-blue-700">Insight</span></Link>
-      </div>
+    <>
+      <div className="flex flex-end bg-white/95 text-stone-900 h-16 items-center p-4 backdrop-blur-[20px] backdrop-opacity-75 sticky top-0 z-50">
+        <div className="flex flex-1 items-center text-xl">
+          <Link href="/">
+            <span className=" text-stone-900">Bull</span>
+            <span className="font-bold text-blue-700">Insight</span>
+          </Link>
+        </div>
 
-      <div className="max-md:block max-md:shrink-0 max-md:cursor-pointer md:hidden : " onClick={toggleMenu}>
-        {isMobileMenuOpen ? (
-          <div className="text-stone-900">
-            <X size={24} />
-          </div>
-        ) : (
-          <div className="text-stone-900">
-            <Menu size={24} />
-          </div>
-          
-        )}
-      </div>
+        <div
+          className="max-md:block max-md:shrink-0 max-md:cursor-pointer md:hidden : "
+          onClick={toggleMenu}
+        >
+          {isMobileMenuOpen ? (
+            <div className="text-stone-900">
+              <X size={24} />
+            </div>
+          ) : (
+            <div className="text-stone-900">
+              <Menu size={24} />
+            </div>
+          )}
+        </div>
 
-      <nav
+        <div className={`absolute w-full h-84 bg-white/95 right-0 top-16 rounded-bl-lg rounded-br-lg backdrop-blur-[20px] backdrop-opacity-75 shadow-lg transform transition-transform duration-900 ease-in-out md:hidden ${isMobileMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+          <ul className="">
+            {links.map((link, index) => (
+              <li key={index}>
+                <Link href={link.href}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* <nav
         className={`${styles.nav} ${
           isMobileMenuOpen ? styles.active : ""
         } ${styles.desktop}`}
@@ -66,7 +79,8 @@ export default function Header() {
             </li>
           ))}
         </ul>
-      </nav>
-    </div>
+      </nav> */}
+      </div>
+    </>
   );
-};
+}
